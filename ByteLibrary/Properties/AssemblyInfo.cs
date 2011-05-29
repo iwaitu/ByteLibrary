f4d87dfbd220317c0d26b2a5adc18196348d7b10
Ben Lakey <benlakey@gmail.com>
2011-05-29 16:20:04 -0700

    Added NuGet spec

--- ByteLibrary/Properties/AssemblyInfo.cs
+++ ByteLibrary/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ByteLibrary")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("My personal library of useful .NET code.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Ben Lakey")]
 [assembly: AssemblyProduct("ByteLibrary")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Ben Lakey 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
