18842fbfdd75bd314291111e9d5f0b6324164b80
Ben Lakey <benlakey@gmail.com>
2011-08-07 22:01:51 -0700

    Added extension method to GraphicsDevice to get the origin of the current screen. Upped version.

---
+++ Byte.Library.Game/Xna/Extensions/GraphicsDeviceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Byte.Library.Game.Xna.Extensions
+{
+    public static class GraphicsDeviceExtensions
+    {
+        public static Vector2 GetScreenOrigin(this GraphicsDevice graphicsDevice)
+        {
+            int screenWidth = graphicsDevice.PresentationParameters.BackBufferWidth;
+            int screenHeight = graphicsDevice.PresentationParameters.BackBufferHeight;
+
+            return new Vector2(screenWidth / 2);
+        }
+    }
+}
