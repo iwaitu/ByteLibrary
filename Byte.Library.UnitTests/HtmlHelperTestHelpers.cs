655103fc2676f097ec8ebfeb7e0e9a9eea3d50b8
Ben Lakey <benlakey@gmail.com>
2012-05-03 18:37:39 -0700

    Added unit tests for HtmlHelperExtensions.

---
+++ Byte.Library.UnitTests/HtmlHelperTestHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Rhino.Mocks;
+
+namespace Byte.Library.UnitTests
+{
+    public static class HtmlHelperTestHelpers
+    {
+        public static HtmlHelper<T> CreateMockHtmlHelper<T>(ViewDataDictionary viewData) where T : class
+        {
+            var mocks = new MockRepository();
+
+            var controllerContext = mocks.DynamicMock<ControllerContext>(
+                mocks.DynamicMock<HttpContextBase>(),
+                new RouteData(),
+                mocks.DynamicMock<ControllerBase>());
+
+            var mockViewContext = mocks.DynamicMock<ViewContext>(
+                controllerContext,
+                mocks.DynamicMock<IView>(),
+                viewData,
+                new TempDataDictionary(),
+                TextWriter.Null);
+
+            var mockViewDataContainer = mocks.DynamicMock<IViewDataContainer>();
+
+            mockViewDataContainer.Expect(v => v.ViewData).Return(viewData);
+
+            return new HtmlHelper<T>(mockViewContext, mockViewDataContainer);
+        }
+    }
+}
---
+++ Byte.Library.UnitTests/Web/HtmlHelperExtensionsTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Byte.Library.Web;
+using Xunit;
+
+namespace Byte.Library.UnitTests.Web
+{
+    public class HtmlHelperExtensionsTests
+    {
+        [Fact]
+        public void Id_generated_matches_property_name()
+        {
+            var htmlHelper = HtmlHelperTestHelpers.CreateMockHtmlHelper<Foo>(new ViewDataDictionary());
+            string id = htmlHelper.IdFor<Foo, string>(f => f.Title);
+
+            Assert.Equal("Title", id);
+        }
+
+        private class Foo
+        {
+            public string Id { get; set; }
+            public string Title { get; set; }
+        }
+    }
+}
